4049970304950027d42b21b3daa53626e02b87ef
vedro-compota <ru-ibn-hottab@yandex.ru>
2017-08-18 17:15:50 +0300

    Последнее изменение

--- index.php
+++ index.php
@@ -1,19 +1,14 @@
 <?php
 
 
-//require_once ("autoload.php"); // автозагрузка классов
+require_once ("autoload.php"); // автозагрузка классов
 require __DIR__ . '/vendor/autoload.php';
 
 
-echo('<pre>'); print_r(spl_autoload_functions()); echo('</pre>');
+Config::debugReporting(); // включаем "строгое" отслеживание ошибок E_ALL
 
-$view = new ItForFree\PhpExamples\MVC\SimpleView();
+$Sess = \core\Session::get();
 
-echo 123;
-//Config::debugReporting(); // включаем "строгое" отслеживание ошибок E_ALL
-//
-//$Sess = \core\Session::get();
-//
-//$route = Url::getRoute();
-////    \DebugPrinter::debug($route, 'путь, найденный URL--ом, передаваемый в Router');
-//$obj = new Router($route);
+$route = Url::getRoute();
+//    \DebugPrinter::debug($route, 'путь, найденный URL--ом, передаваемый в Router');
+$obj = new Router($route);
