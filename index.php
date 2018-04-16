a4112d42d5a50346ead01cb056649cafec4f0c79
vedro-compota <ru-ibn-hottab@yandex.ru>
2018-04-17 00:06:44 +0300

    Зависимости и удаляем закомментированное

--- index.php
+++ index.php
@@ -1,13 +1,10 @@
 <?php
 
 require_once ("autoload.php"); // автозагрузка классов
-//\core\DebugPrinter::debug(\core\Session::get());
-//\core\DebugPrinter::debug(\core\Config::debugReporting());
 
 \Config::debugReporting(); // включаем "строгое" отслеживание ошибок E_ALL
 
 $Sess = \core\Session::get();
 
 $route = \core\mvc\view\Url::getRoute();
-//    \core\DebugPrinter::debug($route, 'путь, найденный URL--ом, передаваемый в Router');
 $obj = new \core\Router($route);
