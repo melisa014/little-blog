b3541c327d0b7c60efd6f775481fe730281f07c3
vedro-compota <ru-ibn-hottab@yandex.ru>
2018-06-04 20:17:57 +0300

    Начинаем использовать объект приложения

--- index.php
+++ index.php
@@ -1,12 +1,15 @@
 <?php
 use ItForFree\SimpleMVC\Application;
 
-require_once ("autoload.php"); // автозагрузка классов
+require_once("autoload.php"); // автозагрузка классов
 
 \Config::debugReporting(); // включаем "строгое" отслеживание ошибок E_ALL
 
 $Sess = \core\Session::get();
 
+//$route = \core\mvc\view\Url::getRoute();
+//$obj = new \core\Router($route);
+
+
 $App = new Application;
 $App->run();
-
