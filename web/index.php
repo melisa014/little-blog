57ac13a2083b4274fefcfb39a9844786db23705f
vedro-compota <ru-ibn-hottab@yandex.ru>
2018-06-11 16:15:25 +0300

    Правим игнор, новый конфиг, продолжаем правки точки входа

--- web/index.php
+++ web/index.php
@@ -4,20 +4,15 @@ use ItForFree\SimpleMVC\Application;
 require_once("autoload.php"); // автозагрузка классов
 
 
-$Sess = Sess;
-
 //$route = \core\mvc\view\Url::getRoute();
 //$obj = new \core\Router($route);
 
 
-require(__DIR__ . '/../application/config.php');
+$localConfig = require(__DIR__ . '/../application/config/web-local.php');
 
 $config = ItForFree\rusphp\PHP\ArrayLib\Merger::mergeRecursivelyWithReplace(
-        require(__DIR__ . '/../application/config/web.php'), 
-        require(__DIR__ . '/../application/config/web-local.php'), $localConfig);
+    require(__DIR__ . '/../application/config/web.php'), 
+    $localConfig);
 
 (new yii\web\Application($config))->run();
 
-
-$App = new Application();
-$App->run();
