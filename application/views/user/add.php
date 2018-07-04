32786650000202b59a9e0d033239d8d3a81714b9
vedro-compota <ru-ibn-hottab@yandex.ru>
2018-07-04 23:22:26 +0300

    Временно открываем возможность создать пользователя и частично правим шаблон

--- application/views/user/add.php
+++ application/views/user/add.php
@@ -3,9 +3,9 @@
 
 <form id="addUser" method="post" action="<?= \ItForFree\SimpleMVC\Url::link("admin/adminusers/add")?>"> 
     <h5>Введите имя пользователя</h5>
-    <input type="text" name="login" value="имя пользователя"><br>
+    <input type="text" name="login" placeholder="имя пользователя"><br>
     <h5>Введите пароль</h5>
-    <input type="text" name="pass" value="пароль"><br>
+    <input type="text" name="pass" placeholder="пароль"><br>
        
     <h5>Права доступа</h5>
     <select name="role"> 
@@ -14,7 +14,7 @@
     </select>
     
     <h5>Введите e-mail</h5>
-    <input type="text" name="email" value="email"><br>
+    <input type="text" name="email" placeholder="адрес электропочты"><br>
     <input type="submit" name="saveNewUser" value="Сохранить">
     <input type="submit" name="cancel" value="Назад"><br>
 </form>
