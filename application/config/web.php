3960440cd206a331a5cee2f5bdd76dd6530facbc
Михаил <toyrik@gmail.com>
2020-08-07 16:14:19 +0300

    Исправляем досадную опечатку в файле конфигурации

--- application/config/web.php
+++ application/config/web.php
@@ -24,7 +24,7 @@ $config = [
             ]
         ],
         'handlers' => [
-            'ItForFree\SimpleMVC\exceptions\SmvcAccessExeption' => \application\handlers\UserExceptionHandler::class,
+            'ItForFree\SimpleMVC\exceptions\SmvcAccessException' => \application\handlers\UserExceptionHandler::class,
             'ItForFree\SimpleMVC\exceptions\SmvcRoutingException' => \application\handlers\UserExceptionHandler::class
         ],
         'user' => [
